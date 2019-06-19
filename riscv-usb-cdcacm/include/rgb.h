01913005cf2e0255bf91e642cbe43157200db236
Sean Cross <sean@xobs.io>
2019-06-19 14:50:37 -0700

    riscv-usb-cdcacm: add example project
    Signed-off-by: Sean Cross <sean@xobs.io>

---
+++ riscv-usb-cdcacm/include/rgb.h
@@ -0,0 +1,10 @@
+#ifndef _RGB_H_
+#define _RGB_H_
+
+void rgb_init(void);
+void rgb_mode_idle(void);
+void rgb_mode_done(void);
+void rgb_mode_writing(void);
+void rgb_mode_error(void);
+
+#endif /* _RGB_H_ */
---
+++ riscv-usb-cdcacm/include/usb.h
@@ -0,0 +1,30 @@
+#ifndef __USB_H
+#define __USB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct usb_setup_request;
+
+void usb_isr(void);
+void usb_init(void);
+void usb_connect(void);
+void usb_disconnect(void);
+
+int usb_irq_happened(void);
+void usb_setup(const struct usb_setup_request *setup);
+int usb_send(const void *data, int total_count);
+void usb_ack_in(void);
+void usb_ack_out(void);
+void usb_err(void);
+int usb_recv(void *buffer, unsigned int buffer_len);
+void usb_poll(void);
+int usb_wait_for_send_done(void);
+void usb_recv_done(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
