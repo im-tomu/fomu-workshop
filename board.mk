2b2685837fc8c0c5ea7fe64147aa6c3e375657d7
umarcor <unai.martinezcorral@ehu.eus>
2020-11-08 02:16:54 +0100

    board.mk: split PCF from PNRFLAGS

--- board.mk
+++ board.mk
@@ -2,20 +2,25 @@
 # require different configurations for yosys and nextpnr.
 # Configuration is performed by setting the environment variable FOMU_REV accordingly.
 ifeq ($(FOMU_REV),evt1)
-YOSYSFLAGS?= -D EVT=1
-PNRFLAGS  ?= --up5k --package sg48 --pcf ../../pcf/fomu-evt2.pcf
+YOSYSFLAGS ?= -D EVT=1
+PNRFLAGS   ?= --up5k --package sg48
+PCF        ?= ../../pcf/fomu-evt2.pcf
 else ifeq ($(FOMU_REV),evt2)
-YOSYSFLAGS?= -D EVT=1
-PNRFLAGS  ?= --up5k --package sg48 --pcf ../../pcf/fomu-evt2.pcf
+YOSYSFLAGS ?= -D EVT=1
+PNRFLAGS   ?= --up5k --package sg48
+PCF        ?= ../../pcf/fomu-evt2.pcf
 else ifeq ($(FOMU_REV),evt3)
-YOSYSFLAGS?= -D EVT=1
-PNRFLAGS  ?= --up5k --package sg48 --pcf ../../pcf/fomu-evt3.pcf
+YOSYSFLAGS ?= -D EVT=1
+PNRFLAGS   ?= --up5k --package sg48
+PCF        ?= ../../pcf/fomu-evt3.pcf
 else ifeq ($(FOMU_REV),hacker)
-YOSYSFLAGS?= -D HACKER=1
-PNRFLAGS  ?= --up5k --package uwg30 --pcf ../../pcf/fomu-hacker.pcf
+YOSYSFLAGS ?= -D HACKER=1
+PNRFLAGS   ?= --up5k --package uwg30
+PCF        ?= ../../pcf/fomu-hacker.pcf
 else ifeq ($(FOMU_REV),pvt)
-YOSYSFLAGS?= -D PVT=1
-PNRFLAGS  ?= --up5k --package uwg30 --pcf ../../pcf/fomu-pvt.pcf
+YOSYSFLAGS ?= -D PVT=1
+PNRFLAGS   ?= --up5k --package uwg30
+PCF        ?= ../../pcf/fomu-pvt.pcf
 else
 $(error Unrecognized FOMU_REV value. must be "evt1", "evt2", "evt3", "pvt", or "hacker")
 endif
