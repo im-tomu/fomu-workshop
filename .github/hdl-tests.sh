8e89c5d8255abe8d04275ee557cdd89f6596f417
umarcor <unai.martinezcorral@ehu.eus>
2021-05-17 08:17:07 +0200

    ci: run hdl tests on all-in-one container

---
+++ .github/hdl-tests.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname "$0")/../hdl
+
+echo '::group::Verilog Blink example'
+(
+	set -x
+	cd verilog/blink
+	make FOMU_REV=pvt blink.bit
+	[ -f blink.bit ]
+)
+echo '::endgroup::'
+
+echo '::group::Verilog Blink (expanded) example for Hacker board'
+(
+	set -x
+	cd verilog/blink-expanded
+	make FOMU_REV=hacker blink.bin
+	[ -f blink.bin ]
+)
+echo '::endgroup::'
+
+echo '::group::Verilog Blink (expanded) example for PVT board'
+(
+	set -x
+	cd verilog/blink-expanded
+	make FOMU_REV=pvt blink.bin
+	[ -f blink.bin ]
+)
+echo '::endgroup::'
+
+echo '::group::VHDL Blink example'
+(
+	set -x
+	cd vhdl/blink
+	make FOMU_REV=pvt blink.bit
+	[ -f blink.bit ]
+)
+echo '::endgroup::'
+
+echo '::group::Mixed HDL Blink example'
+(
+	set -x
+	cd mixed/blink
+	make FOMU_REV=pvt blink.bit
+	[ -f blink.bit ]
+)
+echo '::endgroup::'
