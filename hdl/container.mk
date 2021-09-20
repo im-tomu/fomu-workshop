df78580f7881c84a418d33fd6aca554c2a15ae33
umarcor <unai.martinezcorral@ehu.eus>
2021-09-20 14:14:51 +0200

    use containers from gcr.io/hdl-containers

--- hdl/container.mk
+++ hdl/container.mk
@@ -6,7 +6,7 @@ CONTAINER_ARGS = run \
 	-v /$(PWD)/../../..://wrk \
 	-w //wrk/hdl/$(notdir $(shell dirname $(PWD)))/$(notdir $(PWD))
 
-GHDL    = $(CONTAINER_ENGINE) $(CONTAINER_ARGS) hdlc/ghdl:yosys ghdl
-YOSYS   = $(CONTAINER_ENGINE) $(CONTAINER_ARGS) hdlc/ghdl:yosys yosys
-NEXTPNR = $(CONTAINER_ENGINE) $(CONTAINER_ARGS) hdlc/nextpnr:ice40 nextpnr-ice40
-ICEPACK = $(CONTAINER_ENGINE) $(CONTAINER_ARGS) hdlc/icestorm icepack
+GHDL    = $(CONTAINER_ENGINE) $(CONTAINER_ARGS) gcr.io/hdl-containers/ghdl/yosys ghdl
+YOSYS   = $(CONTAINER_ENGINE) $(CONTAINER_ARGS) gcr.io/hdl-containers/ghdl/yosys yosys
+NEXTPNR = $(CONTAINER_ENGINE) $(CONTAINER_ARGS) gcr.io/hdl-containers/nextpnr/ice40 nextpnr-ice40
+ICEPACK = $(CONTAINER_ENGINE) $(CONTAINER_ARGS) gcr.io/hdl-containers/icestorm icepack
